271c928ef1c5282f2bba40bd58554f0ce7bdcfdf
pandeysumit8085-cmd <pandeysumit8085@gmail.com>
2026-06-28 12:45:36 +0530

    Add event listener for enter button
    Java script codes are here

---
+++ Script.js
@@ -0,0 +1,12 @@
+// Enter Website Button
+
+document.getElementById("enterBtn").addEventListener("click", function () {
+
+    alert("Welcome to Aditya Agrijunction");
+
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: "smooth"
+    });
+
+});
